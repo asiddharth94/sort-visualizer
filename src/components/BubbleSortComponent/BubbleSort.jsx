a2178ddabdfcc0dae35fbc1877d9ad7c2ca2c085
asiddharth94 <49415269+asiddharth94@users.noreply.github.com>
2021-07-18 18:43:57 +0530

    Added bubble sort logic

---
+++ src/components/BubbleSortComponent/BubbleSort.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from "react";
+
+import "./BubbleSort.css";
+
+const getRandomArray = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+function BubbleSort() {
+  const [bubbleArray, setBubbleArray] = useState([]);
+
+  useEffect(() => {
+    getArray();
+  }, []);
+
+  function bubbleSort(bubbleArray) {
+    let sortedArray = [...bubbleArray];
+    for (let i = 0; i < sortedArray.length; i++) {
+      for (let j = 0; j < sortedArray.length - i - 1; j++) {
+        if (sortedArray[j] > sortedArray[j + 1]) {
+          let temp = sortedArray[j];
+          sortedArray[j] = sortedArray[j + 1];
+          sortedArray[j + 1] = temp;
+        }
+      }
+    }
+  }
+
+  const getArray = () => {
+    const array = [];
+    for (let i = 0; i < 100; i++) {
+      array[i] = getRandomArray(20, 500);
+    }
+    setBubbleArray(array);
+  };
+
+  return (
+    <div className="bubble-sort">
+      <button
+        onClick={() => {
+          bubbleSort(bubbleArray);
+        }}
+      >
+        SORT
+      </button>
+      {bubbleArray.map((elem, index) => (
+        <div
+          key={index}
+          className="array-bar"
+          style={{ height: `${elem}px` }}
+        ></div>
+      ))}
+    </div>
+  );
+}
+
+export default BubbleSort;
---
+++ src/components/SortOptionComponent/SortOption.jsx
@@ -0,0 +1,17 @@
+import "./SortOption.css";
+
+function SortOption() {
+  return (
+    <div className="sort-option">
+      <ul>
+        <li>Bubble</li>
+        <li>Insertion</li>
+        <li>Selection</li>
+        <li>Merge</li>
+        <li>Quick</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SortOption;
